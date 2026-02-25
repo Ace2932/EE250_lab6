327ef8019a7500d226e86770c43a928f1a63f79e
Aiden Fox <asfox@usc.edu>
2026-02-24 17:41:42 -0800

    following lab guidelines

--- grovepi_sensors.py
+++ grovepi_sensors.py
@@ -6,27 +6,40 @@ from grove_rgb_lcd import *
 
 # Grove Ultrasonic Ranger connectd to digital port 2
 ultrasonic_ranger = 2
-# potentiometer connected to analog port A0 as input
-potentiometer = 0
-grovepi.pinMode(potentiometer,"INPUT")
+# Rotary angle sensor connected to analog port A1 (output [0, 1023])
+rotary_sensor = 1
+grovepi.pinMode(rotary_sensor, "INPUT")
 
-# clear lcd screen  before starting main loop
+# Ultrasonic range observed in testing [0, 517] cm
+ULTRASONIC_MAX = 517
+ROTARY_MAX = 1023
+
+# Clear LCD screen before starting main loop
 setText("")
 
 while True:
   try:
     # TODO:read distance value from Ultrasonic Ranger and print distance on LCD
     distance = grovepi.ultrasonicRead(ultrasonic_ranger)
-    print(distance)
-  
 
-    # TODO: read threshold from potentiometer
-    threshold = grovepi.analogRead(potentiometer)
-    print(threshold)
-    
-    # TODO: format LCD text according to threshhold
-    text = f"Distance: {distance}cm, Threshold: {threshold}"
-    setText(text)
-    
+    # Read rotary [0, 1023], map to threshold range [0, 517] for comparison with raw ultrasonic
+    rotary_value = grovepi.analogRead(rotary_sensor)
+    threshold = int(rotary_value * ULTRASONIC_MAX / ROTARY_MAX)
+
+    # Object present when raw ultrasonic output falls below threshold
+    object_present = distance < threshold
+
+    # Top line: current threshold value + space, then " OBJ PRES" if object present
+    line1 = f"{threshold} "
+    if object_present:
+      line1 += " OBJ PRES"
+    line1 = (line1 + " " * 16)[:16]
+
+    # Bottom line: current raw ultrasonic ranger output
+    line2 = (str(distance) + " " * 16)[:16]
+
+    setText_norefresh(line1 + "\n" + line2)
+
   except IOError:
     print("Error")
+  time.sleep(0.1)
